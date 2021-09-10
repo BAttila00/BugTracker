51d19e9d69a896d0435b73c220e609440b6ca78a
Attila Bruder <bruderattila@gmail.com>
2021-09-10 17:39:50 +0200

    DbContext Hozzáadása. (BugTrackerDbContext)

---
+++ BugTracker.Dal/BugTrackerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Dal {
+    class BugTrackerDbContext : DbContext {
+        // ezen options-el az adatbázis-kapcsolat konfigurációs beállításait (timeout, connection string) állíthatjuk be.
+        public BugTrackerDbContext(DbContextOptions options) : base(options) {
+
+        }
+    }
+}
