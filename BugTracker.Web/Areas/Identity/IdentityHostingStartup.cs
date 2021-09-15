e304bbd3c2ea07ddaa29fb1bad9a42a529ff23ad
Attila Bruder <bruderattila@gmail.com>
2021-09-15 20:52:34 +0200

    Identity pages scaffolding.

---
+++ BugTracker.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BugTracker.Dal;
+using BugTracker.Dal.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BugTracker.Web.Areas.Identity.IdentityHostingStartup))]
+namespace BugTracker.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
