3e3a4421fd700997ac4651b758ce161b6e6eea1b
Attila Bruder <bruderattila@gmail.com>
2021-10-09 19:09:24 +0200

    Eltávolítottam a nem kellő mezőket a Users listázó oldalról. Valamint megjelenítem a szerepköröket is.

--- BugTracker.Web/Pages/Users/Details.cshtml.cs
+++ BugTracker.Web/Pages/Users/Details.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BugTracker.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +13,17 @@ namespace BugTracker.Web.Pages.Users
     public class DetailsModel : PageModel
     {
         private readonly BugTracker.Dal.BugTrackerDbContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public DetailsModel(BugTracker.Dal.BugTrackerDbContext context)
+        public DetailsModel(BugTracker.Dal.BugTrackerDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public User User { get; set; }
+
+        public string UserRoles { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -33,6 +38,10 @@ namespace BugTracker.Web.Pages.Users
             {
                 return NotFound();
             }
+
+            var roles = await _userManager.GetRolesAsync(User);
+            UserRoles = string.Join(", ", roles);
+
             return Page();
         }
     }
--- BugTracker.Web/Pages/Users/Index.cshtml.cs
+++ BugTracker.Web/Pages/Users/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BugTracker.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,12 @@ namespace BugTracker.Web.Pages.Users
     public class IndexModel : PageModel
     {
         private readonly BugTracker.Dal.BugTrackerDbContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public IndexModel(BugTracker.Dal.BugTrackerDbContext context)
+        public IndexModel(BugTracker.Dal.BugTrackerDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<User> User { get;set; }
@@ -23,6 +26,11 @@ namespace BugTracker.Web.Pages.Users
         public async Task OnGetAsync()
         {
             User = await _context.Users.ToListAsync();
+        }
+
+        public async Task<string> GetUserRoles(User user) {
+            var roles =  await _userManager.GetRolesAsync(user);
+            return string.Join(", ", roles);
         }
     }
 }
