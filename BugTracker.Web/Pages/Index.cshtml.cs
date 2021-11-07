e233965331c216fd4d1df7a8ef14d449361165e5
Attila Bruder <bruderattila@gmail.com>
2021-11-07 21:18:11 +0100

    Fő index oldal kialakítása. MyView nézet. Collapse, DataTable.

--- BugTracker.Web/Pages/Index.cshtml.cs
+++ BugTracker.Web/Pages/Index.cshtml.cs
@@ -1,21 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BugTracker.Dal;
+using BugTracker.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BugTracker.Web.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        private readonly BugTrackerDbContext _context;
+
+        public IList<Issue> Issues { get; set; }
+        public IList<Issue> IssuesUnresolvedAssignedToMe { get; set; }
+        public IList<Issue> IssuesUnassigned { get; set; }
+        public IList<Issue> IssuesResolvedCreatedByMe { get; set; }
+        public IList<Issue> IssuesRecentlyModified { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, BugTrackerDbContext context) {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet() {
+            //A bejelentkezett user id-ja
+            int userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
+            Issues = _context.Issues
+                .Include(i => i.AssignedTo)
+                .Include(i => i.Creator)
+                .Include(i => i.ModifiedBy)
+                .Include(i => i.Project).ToList();
+
+            IssuesUnresolvedAssignedToMe = Issues.Where(i => i.AssignedToId == userId && i.IssueStatus != IssueStatus.Resolved && i.IssueStatus != IssueStatus.Closed).ToList();
+            IssuesUnassigned = Issues.Where(i => i.IssueStatus == IssueStatus.Unassigned).ToList();
+            IssuesResolvedCreatedByMe = Issues.Where(i => i.IssueStatus == IssueStatus.Resolved && i.CreatorId == userId).ToList();
+            IssuesRecentlyModified = Issues.Where(i => ((DateTime.Now - i.ModifiedOn).TotalDays < 5)).ToList();
         }
     }
 }
