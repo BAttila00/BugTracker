9521c31ad9c87de9f05f3885b4cc24e136463ce8
Attila Bruder <bruderattila@gmail.com>
2021-11-21 20:50:16 +0100

    Komment írója legyen setnull, ha töröltük a user-t. Előző kommitban van a setnull állítás, itt meg a db migráció.

---
+++ BugTracker.Dal/Migrations/20211121192753_SetNullUserOnComment.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Dal.Migrations
+{
+    public partial class SetNullUserOnComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_User",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_User",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_User",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_User",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
